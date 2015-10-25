4ed8a5f7e28134a9099eb1ac2b56a49180de5896
maxdignan <maxdignan@gmail.com>
2015-10-24 21:19:20 -0400

    init template app

---
+++ app/client/js/script.js
@@ -0,0 +1,10 @@
+(function() {
+    var nav = document.getElementById('nav')
+    document.getElementById('openButton').addEventListener('click', function(e) {
+        nav.classList.add('open');
+    });
+
+    document.getElementById('closeButton').addEventListener('click', function(e) {
+        nav.classList.remove('open');
+    });
+})();
